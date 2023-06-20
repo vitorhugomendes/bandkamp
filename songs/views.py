b149442000ec333d05c4d791d987135151c19614
Vitor Hugo Mendes <vitor_hugorj@hotmail.com>
2023-06-20 13:03:38 -0300

    feat: Tarefa 3 finalizada

--- songs/views.py
+++ songs/views.py
@@ -1,4 +1,4 @@
-from rest_framework.views import APIView, Response, status
+from rest_framework.generics import ListCreateAPIView
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.pagination import PageNumberPagination
@@ -8,29 +8,17 @@ from .models import Song
 from .serializers import SongSerializer
 
 
-class SongView(APIView, PageNumberPagination):
+class SongView(ListCreateAPIView, PageNumberPagination):
     authentication_classes = [JWTAuthentication]
     permission_classes = [IsAuthenticatedOrReadOnly]
 
-    def get(self, request, pk):
-        """
-        Obtençao de musicas
-        """
-        songs = Song.objects.filter(album_id=pk)
+    serializer_class = SongSerializer
 
-        result_page = self.paginate_queryset(songs, request)
-        serializer = SongSerializer(result_page, many=True)
+    def get_queryset(self):
+        album_pk = self.kwargs["pk"]
+        return Song.objects.filter(album_id=album_pk)
 
-        return self.get_paginated_response(serializer.data)
-
-    def post(self, request, pk):
-        """
-        Criaçao de musica
-        """
-        album = get_object_or_404(Album, pk=pk)
-
-        serializer = SongSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save(album=album)
-
-        return Response(serializer.data, status.HTTP_201_CREATED)
+    def perform_create(self, serializer):
+        album_pk = self.kwargs["pk"]
+        album = get_object_or_404(Album, pk=album_pk)
+        return serializer.save(album=album)
--- users/views.py
+++ users/views.py
@@ -1,9 +1,8 @@
 from rest_framework.views import APIView, Request, Response, status
-from rest_framework.generics import CreateAPIView
+from rest_framework.generics import CreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework_simplejwt.tokens import RefreshToken
 from django.contrib.auth import authenticate
-from django.shortcuts import get_object_or_404
 from .models import User
 from .serializers import UserSerializer, LoginSerializer
 from .permissions import IsAccountOwner
@@ -39,44 +38,9 @@ class UserView(CreateAPIView):
     serializer_class = UserSerializer
 
 
-class UserDetailView(APIView):
+class UserDetailView(RetrieveUpdateDestroyAPIView):
     authentication_classes = [JWTAuthentication]
     permission_classes = [IsAccountOwner]
 
-    def get(self, request: Request, pk: int) -> Response:
-        """
-        Obtençao de usuário
-        """
-        user = get_object_or_404(User, pk=pk)
-
-        self.check_object_permissions(request, user)
-
-        serializer = UserSerializer(user)
-
-        return Response(serializer.data)
-
-    def patch(self, request: Request, pk: int) -> Response:
-        """
-        Atualização de usuário
-        """
-        user = get_object_or_404(User, pk=pk)
-
-        self.check_object_permissions(request, user)
-
-        serializer = UserSerializer(user, data=request.data, partial=True)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-
-        return Response(serializer.data)
-
-    def delete(self, request: Request, pk: int) -> Response:
-        """
-        Deleçao de usuário
-        """
-        user = get_object_or_404(User, pk=pk)
-
-        self.check_object_permissions(request, user)
-
-        user.delete()
-
-        return Response(status=status.HTTP_204_NO_CONTENT)
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
